1948abb6e85edc2f7b7b4c894cbfc4fff085b048
Kaori Kawakami <k.kawakami08@gmail.com>
2022-10-19 01:11:43 -0700

    hero and footer section elements and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element = {<HomePage />}/>
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import './HomePage.css'
+import heroImg from '../images/hero-img.jpg'
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className="home-container">
+      <div className="hero-img" style={{backgroundImage: `url(${heroImg})`}}>
+        <h1>Cozy Up Your Home</h1>
+        <Link to='/shop'>
+          <button className="hero-btn">Shop Now</button>
+        </Link>
+      </div>
+    </div>
   )
 }
