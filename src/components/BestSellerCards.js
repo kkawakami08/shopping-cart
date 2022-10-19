159fb22f8da33ef08be4b2c8da5442af686d13e0
Kaori Kawakami <k.kawakami08@gmail.com>
2022-10-19 15:13:47 -0700

    bestsellercards component functionality

--- src/components/BestSellerCards.js
+++ src/components/BestSellerCards.js
@@ -1,19 +1,46 @@
-import React, {useState} from 'react'
+import React from 'react'
 import anthurium from '../images/products/anthurium-img.jpg'
+import croton from '../images/products/croton-img.jpg'
+import dracaena from '../images/products/dracaena-img.jpg'
+import pothos from '../images/products/pothos-img.jpg'
+import uniqid from 'uniqid'
 
 export default function BestSellerCards() {
   const bestProducts = [{
     title: "Anthurium",
     imgSrc: anthurium, 
     price: "$20",
+    id: uniqid()
+  },{
+    title: "Croton",
+    imgSrc: croton, 
+    price: "$10",
+    id: uniqid()
+  },{
+    title: "Dracaena",
+    imgSrc: dracaena, 
+    price: "$30",
+    id: uniqid()
+  },{
+    title: "Pothos",
+    imgSrc: pothos, 
+    price: "$10",
+    id: uniqid()
   }]
-  return (
-    <div className="card">
-      <img src={bestProducts[0].imgSrc} alt={`${bestProducts[0].title}`} />
+
+  const bestProductsList = bestProducts.map((product) => 
+    <div className="card" key={product.id}>
+      <img src={product.imgSrc} alt={`${product.title}`} />
       <div className="product-des">
-        <p>{bestProducts[0].title}</p>
-        <h3>{bestProducts[0].price}</h3>
+        <p>{product.title}</p>
+        <h3>{product.price}</h3>
       </div>
    </div>
+  );
+
+  return (
+    <>
+    {bestProductsList}
+    </>
   )
 }
