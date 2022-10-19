5baf43f0e8729292ff1c008d15e53ee70a5b5ac6
Kaori Kawakami <k.kawakami08@gmail.com>
2022-10-19 01:18:35 -0700

    fix btn hover css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,10 +8,8 @@ export default function HomePage() {
     <div className="home-container">
       <div className="hero-img" style={{backgroundImage: `url(${heroImg})`}}>
         <h1>Cozy Up Your Home</h1>
-        <Link to='/shop'>
-          <button className="hero-btn">Shop Now</button>
-        </Link>
-      </div>
+          <button id="hero-btn"><Link to='/shop'>Shop Now</Link></button>
+       </div>
     </div>
   )
 }
